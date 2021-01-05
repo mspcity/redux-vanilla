846354ca12be844ef1cb518cd2ddd9c035160558
mspcity <codepark2019@gmail.com>
2021-01-04 23:35:18 -0500

    added action

--- store.js
+++ store.js
@@ -2,7 +2,13 @@ const redux = require('redux');
 console.log(redux)
 
 // actions
-
+//action-types
+const ADD_SUBSCRIBER = 'ADD_SUBSCRIBER'
+const addSubscriber = () => {
+  return {
+    type: 'ADD_SUBSCRIBER'
+  }
+}
 // reducers
 
 //store
