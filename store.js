0c514d111147ab3dac5eda2a52b1337783bdfbab
mspcity <codepark2019@gmail.com>
2021-01-05 22:53:17 -0500

    added reducer

--- store.js
+++ store.js
@@ -10,7 +10,19 @@ const addSubscriber = () => {
   }
 }
 // reducers
-
+const initialState = {
+  subscribers : 365
+}
+const reducer = (state=initialState, action) => {
+  switch(action.type){
+    case ADD_SUBSCRIBER:
+      return {
+        ...state,
+        subscribers: state.subscribers + 1
+      }
+    default: return state;
+  }
+}
 //store
 
 //subscribe - view - dispatch
